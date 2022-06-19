2f2910afeaf807258e14347d73729da9d6ff0d7c
Ray Kochenderfer <rayjkochenderfer@gmail.com>
2022-06-19 17:19:47 -0400

    Add other languages

---
+++ js/index.js
@@ -0,0 +1,47 @@
+// Site: https://www.educative.io/blog/crack-amazon-coding-interview-questions#overview
+console.log('starting run')
+
+/*
+    You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number x. 
+    You have to find x. The input array is not sorted. Look at the below array and give it a try before checking the solution.
+*/
+const findMissing = input => {
+	const sorted = input.sort()
+	for (let i = 0; i < sorted.length; i++) {
+		if (sorted[i] !== i + 1) {
+			return i + 1
+		}
+	}
+	return -1
+}
+
+const findSumOfTwo = (array, targetSum) => {
+	const set = []
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < set.length; j++) {
+			if (set[j] + array[i] === targetSum) {
+				return true
+			}
+		}
+		set.push(i)
+	}
+	return false
+}
+
+const deepCopyArbitraryPointer = (head) => {
+
+}
+
+console.log(`Find Missing: ${findMissing([3, 7, 1, 2, 8, 4, 5])}`)
+console.log(
+	`Find Sum of Two Integers Is Equal To Given Value 10: ${findSumOfTwo(
+		[5, 7, 1, 2, 8, 4, 3],
+		10,
+	)}`,
+)
+console.log(
+	`Find Sum of Two Integers Is Equal To Given Value 19: ${findSumOfTwo(
+		[5, 7, 1, 2, 8, 4, 3],
+		19,
+	)}`,
+)
